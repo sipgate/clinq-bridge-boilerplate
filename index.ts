69f1a15b9e02bd8ce296a5625bd1de00bf340181
Felix Gladisch <gladisch@sipgate.de>
2018-02-20 11:53:05 +0100

    chore: clinq-crm-bridge 2.0.1

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
-import { BridgeImplementation, Contact, start as startServer } from "clinq-crm-bridge";
+import { CrmAdapter, Contact, start } from "clinq-crm-bridge";
 
-class MyCrmAdapter implements BridgeImplementation {
+class MyCrmAdapter implements CrmAdapter {
 
 	public async getContacts(token: string): Promise<Contact[]> {
 		// TODO: Fetch contacts from CRM
 		const contacts: Contact[] = await Promise.resolve([
-			{ name: "Benjamin Kluck", phoneNumbers: ["+4915799912345"] }
+			{
+				name: "Benjamin Kluck",
+				phoneNumbers: [{
+					label: "Mobile",
+					phoneNumber: "+4915799912345"
+				}]
+			}
 		]);
 
 		return contacts;
 	}
-
 }
 
-startServer(new MyCrmAdapter());
+start(new MyCrmAdapter());
