510f4c6b2a20af9695f8b990bce33ef1eb3d8bd9
Felix Gladisch <gladisch@sipgate.de>
2018-03-21 14:45:42 +0100

    style: prettier

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { CrmAdapter, CrmConfig, Contact, start } from "clinq-crm-bridge";
 import { Request } from "express";
 
 class MyCrmAdapter implements CrmAdapter {
-
 	/**
 	 * TODO: Fetch contacts from CRM using config.apiKey and config.apiUrl or throw on error
 	 */
@@ -10,10 +9,12 @@ class MyCrmAdapter implements CrmAdapter {
 		const contacts: Contact[] = await Promise.resolve([
 			{
 				name: "Benjamin Kluck",
-				phoneNumbers: [{
-					label: "Mobile",
-					phoneNumber: "+4915799912345"
-				}]
+				phoneNumbers: [
+					{
+						label: "Mobile",
+						phoneNumber: "+4915799912345"
+					}
+				]
 			}
 		]);
 
@@ -26,7 +27,9 @@ class MyCrmAdapter implements CrmAdapter {
 	 * Users will be redirected here to authorize CLINQ.
 	 */
 	public async getOAuth2RedirectUrl(): Promise<string> {
-		const redirectUrl = await Promise.resolve("https://crm.example.com/oauth2/authorize");
+		const redirectUrl = await Promise.resolve(
+			"https://crm.example.com/oauth2/authorize"
+		);
 		return redirectUrl;
 	}
 
@@ -38,7 +41,6 @@ class MyCrmAdapter implements CrmAdapter {
 	 * and return it as 'apiKey'
 	 */
 	public async handleOAuth2Callback(req: Request): Promise<CrmConfig> {
-
 		// EXAMPLE:
 		// const { code } = req.query;
 		// const query = queryString.stringify({ code });
@@ -54,7 +56,6 @@ class MyCrmAdapter implements CrmAdapter {
 		});
 		return crmConfig;
 	}
-
 }
 
 start(new MyCrmAdapter());
